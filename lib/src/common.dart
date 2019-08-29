b577cb595bbb9426e158a55253e5455e08f9efb4
Julian <noreply+julian@objectbox.io>
2019-08-29 12:20:26 +0200

    Fix memory leak in OBXCommon.version

--- lib/src/common.dart
+++ lib/src/common.dart
@@ -6,6 +6,10 @@ class OBXCommon {
     static List<int> version() {
         Pointer<Int32> majorPtr = allocate(),  minorPtr = allocate(), patchPtr = allocate();
         bindings.obx_version(majorPtr, minorPtr, patchPtr);
-        return [majorPtr.load<int>(), minorPtr.load<int>(), patchPtr.load<int>()];
+        var ret = [majorPtr.load<int>(), minorPtr.load<int>(), patchPtr.load<int>()];
+        majorPtr.free();
+        minorPtr.free();
+        patchPtr.free();
+        return ret;
     }
 }
