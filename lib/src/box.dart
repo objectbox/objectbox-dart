e2f1231edb12b71e86f553a72a415214b4aa9170
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-10-22 08:09:59 +0200

    remove leading underscore from box methods' local vars

--- lib/src/box.dart
+++ lib/src/box.dart
@@ -167,22 +167,22 @@ class Box<T> {
   QueryBuilder query(Condition qc) => QueryBuilder<T>(_store, _fbManager, _modelEntity.id.id, qc);
 
   int count({int limit = 0}) {
-    Pointer<Uint64> _count = Pointer<Uint64>.allocate();
+    Pointer<Uint64> count = Pointer<Uint64>.allocate();
     try {
-      checkObx(bindings.obx_box_count(_cBox, limit, _count));
-      return _count.load<int>();
+      checkObx(bindings.obx_box_count(_cBox, limit, count));
+      return count.load<int>();
     } finally {
-      _count.free();
+      count.free();
     }
   }
 
   bool isEmpty() {
-    Pointer<Uint8> _isEmpty = Pointer<Uint8>.allocate();
+    Pointer<Uint8> isEmpty = Pointer<Uint8>.allocate();
     try {
-      checkObx(bindings.obx_box_is_empty(_cBox, _isEmpty));
-      return _isEmpty.load<int>() > 0 ? true : false;
+      checkObx(bindings.obx_box_is_empty(_cBox, isEmpty));
+      return isEmpty.load<int>() > 0 ? true : false;
     } finally {
-      _isEmpty.free();
+      isEmpty.free();
     }
   }
 
@@ -199,24 +199,24 @@ class Box<T> {
   }
 
   int removeMany(List<int> ids) {
-    Pointer<Uint64> _removedIds = Pointer<Uint64>.allocate();
+    Pointer<Uint64> removedIds = Pointer<Uint64>.allocate();
     try {
       return OBX_id_array.executeWith(ids, (ptr) {
-        checkObx(bindings.obx_box_remove_many(_cBox, ptr, _removedIds));
-        return _removedIds.load<int>();
+        checkObx(bindings.obx_box_remove_many(_cBox, ptr, removedIds));
+        return removedIds.load<int>();
       });
     } finally {
-      _removedIds.free();
+      removedIds.free();
     }
   }
 
   int removeAll() {
-    Pointer<Uint64> _removedItems = Pointer<Uint64>.allocate();
+    Pointer<Uint64> removedItems = Pointer<Uint64>.allocate();
     try {
-      checkObx(bindings.obx_box_remove_all(_cBox, _removedItems));
-      return _removedItems.load<int>();
+      checkObx(bindings.obx_box_remove_all(_cBox, removedItems));
+      return removedItems.load<int>();
     } finally {
-      _removedItems.free();
+      removedItems.free();
     }
   }
 
