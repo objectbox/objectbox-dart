0f03a1d63417fc097233e80a2a9797e391dae6e8
Ivan Dlugos <noreply+ivan@objectbox.io>
2019-12-18 16:39:53 +0100

    add iOS support using a release archive from objectbox-swift

---
+++ ios/download-framework.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+obxSwiftVersion="1.2.0"
+
+dir=$(dirname "$0")
+
+url="https://github.com/objectbox/objectbox-swift/releases/download/v${obxSwiftVersion}/ObjectBox-framework-${obxSwiftVersion}.zip"
+zip="${dir}/fw.zip"
+
+curl --location --fail --output "${zip}" "${url}"
+
+rm -rf "${dir}/Carthage"
+unzip "${zip}" -d "${dir}" \
+  "Carthage/Build/iOS/ObjectBox.framework/Headers/*" \
+  "Carthage/Build/iOS/ObjectBox.framework/ObjectBox" \
+  "Carthage/Build/iOS/ObjectBox.framework/Info.plist"
+
+rm "${zip}"
