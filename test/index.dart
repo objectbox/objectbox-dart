b1766d5283ba4cc8530cc27768719af8244a1d30
Ivan Dlugos <noreply+ivan@objectbox.io>
2020-07-13 11:34:38 +0200

    remove resurrected test/index.dart

--- test/index.dart
+++
@@ -1,9 +0,0 @@
-import 'box_test.dart' as box_test;
-import 'query_test.dart' as query_test;
-import 'query_property_test.dart' as query_property_test;
-
-void main() {
-  box_test.main();
-  query_test.main();
-  query_property_test.main();
-}
