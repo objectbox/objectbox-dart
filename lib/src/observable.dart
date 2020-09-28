6aacbf9707134ecc146fa51504269d7940545637
Jasm Sison <jasm.sison@gmail.com>
2020-09-28 16:23:04 +0200

    fix in case single observer for multiple stores and there is no consensus on entity ids

--- lib/src/observable.dart
+++ lib/src/observable.dart
@@ -14,32 +14,37 @@ typedef Any = void Function(Pointer<Void>, Pointer<Uint32>, int);
 class Observable {
 
   static final anyObserver = <int, Pointer<Void>>{};
-  static final any = <int, Any>{}; // radix? > tree?
+  static final any = <int, Map<int, Any>>{};
 
   // sync:true -> ObjectBoxException: 10001 TX is not active anymore: #101
   static final controller = StreamController<int>.broadcast();
 
+  // The user_data is used to pass the store ptr address
+  // in case there is no consensus on the entity id between stores
   static void _anyCallback(Pointer<Void> user_data, Pointer<Uint32> mutated_ids, int mutated_count) {
+    final storeAddress = user_data.address;
     for(var i=0; i<mutated_count; i++) {
       // call schema's callback
-      if (any.containsKey(mutated_ids[i])) {
-        any[mutated_ids[i]](user_data, mutated_ids, mutated_count);
+      if (any.containsKey(storeAddress) && any[storeAddress].containsKey(mutated_ids[i])) {
+        any[storeAddress][mutated_ids[i]](user_data, mutated_ids, mutated_count);
       }
     }
   }
 
   static void subscribe(Store store) {
     final callback = Pointer.fromFunction<obx_observer_t>(_anyCallback);
-    anyObserver[store.ptr.address] = bindings.obx_observe(store.ptr, callback, store.ptr);
+    final storePtr = store.ptr;
+    anyObserver[storePtr.address] = bindings.obx_observe(storePtr, callback, storePtr);
   }
 
   // #53 ffi:Pointer finalizer
   static void unsubscribe(Store store) {
-    if (!anyObserver.containsKey(store.ptr.address)) {
+    final storeAddress = store.ptr.address;
+    if (!anyObserver.containsKey(storeAddress)) {
       return;
     }
-    bindings.obx_observer_close(anyObserver[store.ptr.address]);
-    anyObserver.remove(store.ptr.address);
+    bindings.obx_observer_close(anyObserver[storeAddress]);
+    anyObserver.remove(storeAddress);
   }
 }
 
@@ -50,12 +55,17 @@ extension ObservableStore on Store {
 
 extension Streamable<T> on Query<T> {
   void _setup() {
-    if (!Observable.anyObserver.containsKey(store.ptr)) {
+
+    final storePtr = store.ptr;
+
+    if (!Observable.anyObserver.containsKey(storePtr)) {
       store.subscribe();
     }
 
-    // Assume consensus on entityId over all available Stores
-    Observable.any[entityId] ??= (u, _, __) {
+    final storeAddress = storePtr.address;
+
+    Observable.any[storeAddress] ??= <int, Any>{};
+    Observable.any[storeAddress][entityId] ??= (u, _, __) {
       // dummy value to trigger an event
       Observable.controller.add(u.address);
     };
