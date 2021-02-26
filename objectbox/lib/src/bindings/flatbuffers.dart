99d772fe43f35cdc3cde4dbf2bab47462f9d836f
Ivan Dlugos <noreply+ivan@objectbox.io>
2021-02-26 18:22:43 +0100

    flatbuffers allocator - further checks and minor updates

--- objectbox/lib/src/bindings/flatbuffers.dart
+++ objectbox/lib/src/bindings/flatbuffers.dart
@@ -11,7 +11,7 @@ import '../../flatbuffers/flat_buffers.dart' as fb;
 // Note: touch this file with caution, it's a hotspot and optimized for our use.
 
 class BuilderWithCBuffer {
-  final _allocator = _Allocator();
+  final _allocator = Allocator();
   final int _initialSize;
   final int _resetIfLargerThan;
 
@@ -36,23 +36,25 @@ class BuilderWithCBuffer {
     }
   }
 
-  void clear() => _allocator.close();
+  void clear() => _allocator.freeAll();
+
+  Allocator get allocator => _allocator;
 }
 
 // FFI signature
 typedef _dart_memset = void Function(Pointer<Uint8>, int, int);
 typedef _c_memset = Void Function(Pointer<Uint8>, Int32, IntPtr);
 
-_dart_memset _memset;
+_dart_memset fbMemset;
 
-class _Allocator extends fb.Allocator {
+class Allocator extends fb.Allocator {
   // We may, in practice, have only two active allocations: one used and one
   // for resizing. Therefore, we use a ring buffer of a fixed size (2).
   // Note: keep the list fixed-size - those are much faster to index
   final _allocs = List<Pointer<Uint8>>.filled(2, nullptr, growable: false);
 
-  // only for testing:
-  // final _data = List<ByteData /*?*/ >.filled(2, null, growable: false);
+  // only used for sanity checks:
+  final _data = List<ByteData /*?*/ >.filled(2, null, growable: false);
 
   // currently used allocator index
   int _index = 0;
@@ -73,21 +75,16 @@ class _Allocator extends fb.Allocator {
     _capacity = size;
     final index = _flipIndex();
     _allocs[index] = f.allocate<Uint8>(count: size);
-
-    // only for testing:
-    // _data[index] = ByteData.view(_allocs[index].asTypedList(size).buffer);
-    // return _data[index];
-
-    return ByteData.view(_allocs[index].asTypedList(size).buffer);
+    _data[index] = ByteData.view(_allocs[index].asTypedList(size).buffer);
+    return _data[index];
   }
 
   @override
   void deallocate(ByteData data) {
     final index = _index == 0 ? 1 : 0; // get the other index
 
-    // only for testing:
-    // assert(_data[index] == data);
-    // clear(data, true);
+    // only used for sanity checks:
+    assert(_data[index] == data);
 
     f.free(_allocs[index]);
     _allocs[index] = nullptr;
@@ -95,17 +92,17 @@ class _Allocator extends fb.Allocator {
 
   @override
   void clear(ByteData data, bool _) {
-    if (_memset == null) {
+    if (fbMemset == null) {
       if (Platform.isWindows) {
         try {
           // DynamicLibrary.process() is not available on Windows, let's load a
           // lib that defines 'memset()' it - should be mscvr100 or mscvrt DLL.
           // mscvr100.dll is in the frequently installed MSVC Redistributable.
-          _memset = DynamicLibrary.open('msvcr100.dll')
+          fbMemset = DynamicLibrary.open('msvcr100.dll')
               .lookupFunction<_c_memset, _dart_memset>('memset');
         } catch (_) {
           // fall back if we can't load a native memset()
-          _memset = (Pointer<Uint8> ptr, int byte, int size) {
+          fbMemset = (Pointer<Uint8> ptr, int byte, int size) {
             final bytes = ptr.cast<Uint8>();
             for (var i = 0; i < size; i++) {
               bytes[i] = byte;
@@ -113,15 +110,19 @@ class _Allocator extends fb.Allocator {
           };
         }
       } else {
-        _memset = DynamicLibrary.process()
+        fbMemset = DynamicLibrary.process()
             .lookupFunction<_c_memset, _dart_memset>('memset');
       }
     }
+
+    // only used for sanity checks:
+    assert(_data[_index] == data);
     assert(_allocs[_index].address != 0);
-    _memset(_allocs[_index], 0, data.lengthInBytes);
+
+    fbMemset(_allocs[_index], 0, data.lengthInBytes);
   }
 
-  void close() {
+  void freeAll() {
     if (_allocs[0].address != 0) f.free(_allocs[0]);
     if (_allocs[1].address != 0) f.free(_allocs[1]);
   }
