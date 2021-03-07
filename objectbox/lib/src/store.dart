96fe897936c72ad29a2addd27c29ecb2ddd617b1
Ivan Dlugos <noreply+ivan@objectbox.io>
2021-03-07 19:04:36 +0100

    make sure pub.dev doesn't think objectbox supports Flutter web just yet

--- objectbox/lib/src/store.dart
+++ objectbox/lib/src/store.dart
@@ -1,1 +1,5 @@
-export 'native/store.dart' if (dart.library.html) 'web/store.dart';
+// Web support is currently early-stage WIP, prevent pub.dev recognizing this
+// objectbox as already web-capable. See tracking issue #185.
+// export 'native/store.dart' if (dart.library.html) 'web/store.dart';
+
+export 'native/store.dart';
