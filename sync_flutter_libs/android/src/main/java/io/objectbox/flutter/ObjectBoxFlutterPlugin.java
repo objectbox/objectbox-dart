ce3caa97a214dd67d39951e3177e99bfd3693d5c
Ivan Dlugos <noreply+ivan@objectbox.io>
2021-03-31 15:28:25 +0200

    flutter plugins - remove deprecated APIs
    closes #147

--- sync_flutter_libs/android/src/main/java/io/objectbox/flutter/ObjectBoxFlutterPlugin.java
+++ sync_flutter_libs/android/src/main/java/io/objectbox/flutter/ObjectBoxFlutterPlugin.java
@@ -7,7 +7,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** ObjectBoxFlutterPlugin */
 public class ObjectBoxFlutterPlugin implements FlutterPlugin, MethodCallHandler {
@@ -19,22 +18,8 @@ public class ObjectBoxFlutterPlugin implements FlutterPlugin, MethodCallHandler 
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "objectbox");
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "objectbox");
     channel.setMethodCallHandler(this);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "objectbox");
-    channel.setMethodCallHandler(new ObjectBoxFlutterPlugin());
   }
 
   @Override
