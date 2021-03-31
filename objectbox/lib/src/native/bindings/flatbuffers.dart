43c7500e165427bcec931de0e8d7485c4f625e16
Ivan Dlugos <noreply+ivan@objectbox.io>
2021-03-31 16:00:36 +0200

    memset fallback - let's be more generic and actually fill with the given value instead of zeroes

--- objectbox/lib/src/native/bindings/flatbuffers.dart
+++ objectbox/lib/src/native/bindings/flatbuffers.dart
@@ -109,7 +109,7 @@ class Allocator extends fb.Allocator {
         } catch (_) {
           // fall back if we can't load a native memset()
           fbMemset = (Pointer<Uint8> ptr, int byte, int size) =>
-              ptr.cast<Uint8>().asTypedList(size).fillRange(0, size, 0);
+              ptr.cast<Uint8>().asTypedList(size).fillRange(0, size, byte);
         }
       } else {
         fbMemset = DynamicLibrary.process()
