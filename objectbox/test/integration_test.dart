5c48d215461c08f77bb348f713ae2f1b5c9b2a75
Ivan Dlugos <noreply+ivan@objectbox.io>
2021-05-13 10:45:52 +0200

    remove outdated integration_test

--- objectbox/test/integration_test.dart
+++
@@ -1,7 +0,0 @@
-import 'package:test/test.dart';
-import 'package:objectbox/integration_test.dart';
-
-void main() {
-  test('int64', IntegrationTest.int64);
-  test('model', IntegrationTest.model);
-}
