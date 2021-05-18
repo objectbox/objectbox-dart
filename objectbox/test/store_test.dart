49e457e65b2f80c45094dd52f3aaf3618fd4e358
Uwe Trottmann <13865709+greenrobot-team@users.noreply.github.com>
2021-05-18 08:51:01 +0200

    Async create/close store multiple times test.

--- objectbox/test/store_test.dart
+++ objectbox/test/store_test.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:io';
 
 import 'package:test/test.dart';
@@ -11,6 +12,18 @@ void main() {
 
     for (var i = 0; i < 1000; i++) {
       final store = Store(getObjectBoxModel(), directory: dir.path);
+      store.close();
+    }
+  });
+
+  test('store create close multiple async', () async {
+    final dir = Directory('testdata-store');
+    if (dir.existsSync()) dir.deleteSync(recursive: true);
+
+    for (var i = 0; i < 100; i++) {
+      final createStoreFuture = Future.delayed(const Duration(milliseconds: 1),
+          () => Store(getObjectBoxModel(), directory: dir.path));
+      final store = await createStoreFuture;
       store.close();
     }
   });
