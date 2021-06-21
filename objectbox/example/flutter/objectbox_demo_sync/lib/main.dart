eae528f87effcdfc99644f4f3852071c592a61dd
Markus <markus@greenrobot>
2021-06-21 13:18:17 +0200

    prepare macos application group setting

--- objectbox/example/flutter/objectbox_demo_sync/lib/main.dart
+++ objectbox/example/flutter/objectbox_demo_sync/lib/main.dart
@@ -53,7 +53,8 @@ class ViewModel {
   ViewModel(Directory dir)
       : _store = Store(getObjectBoxModel(),
             directory: dir.path + '/objectbox',
-            macosApplicationGroup: 'objectbox.demo') {
+            macosApplicationGroup: 'objectbox.demo' // replace with a real name
+            ) {
     _box = Box<Note>(_store);
     final qBuilder = _box.query()..order(Note_.date, flags: Order.descending);
     _queryStream = qBuilder.watch(triggerImmediately: true);
