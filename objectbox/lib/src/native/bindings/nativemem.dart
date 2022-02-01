01005feab4772b9b3a1bbe8aef40ad8a7b52bc6c
Uwe Trottmann <13865709+greenrobot-team@users.noreply.github.com>
2022-02-01 08:20:15 +0100

    lints: name types using UpperCamelCase.

--- objectbox/lib/src/native/bindings/nativemem.dart
+++ objectbox/lib/src/native/bindings/nativemem.dart
@@ -5,14 +5,14 @@ import 'dart:io';
 
 /// memset(ptr, value, num) sets the first num bytes of the block of memory
 /// pointed by ptr to the specified value (interpreted as an uint8).
-final _dart_memset memset =
-    _stdlib.lookupFunction<_c_memset, _dart_memset>('memset');
+final _DartMemset memset =
+    _stdlib.lookupFunction<_CMemset, _DartMemset>('memset');
 
-final _dart_memcpy? _memcpyNative = _lookupMemcpyOrNull();
+final _DartMemcpy? _memcpyNative = _lookupMemcpyOrNull();
 
-_dart_memcpy? _lookupMemcpyOrNull() {
+_DartMemcpy? _lookupMemcpyOrNull() {
   try {
-    return _stdlib.lookupFunction<_c_memcpy, _dart_memcpy>('memcpy');
+    return _stdlib.lookupFunction<_CMemcpy, _DartMemcpy>('memcpy');
   } catch (_) {
     return null;
   }
@@ -22,7 +22,7 @@ _dart_memcpy? _lookupMemcpyOrNull() {
 /// and a Dart implementation is used.
 final isMemcpyNotAvailable = _memcpyNative == null;
 
-final _dart_memcpy _memcpyDart = (dest, src, length) {
+final _DartMemcpy _memcpyDart = (dest, src, length) {
   dest
       .asTypedList(length)
       .setAll(0, src.asTypedList(length).getRange(0, length));
@@ -35,14 +35,14 @@ final _dart_memcpy _memcpyDart = (dest, src, length) {
 /// (e.g. for Flutter on iOS 15 simulator), then a Dart implementation is used
 /// to copy data via asTypedList (which is much slower).
 /// https://github.com/objectbox/objectbox-dart/issues/313
-final _dart_memcpy memcpy = _memcpyNative ?? _memcpyDart;
+final _DartMemcpy memcpy = _memcpyNative ?? _memcpyDart;
 
 // FFI signature
-typedef _dart_memset = void Function(Pointer<Uint8>, int, int);
-typedef _c_memset = Void Function(Pointer<Uint8>, Int32, IntPtr);
+typedef _DartMemset = void Function(Pointer<Uint8>, int, int);
+typedef _CMemset = Void Function(Pointer<Uint8>, Int32, IntPtr);
 
-typedef _dart_memcpy = void Function(Pointer<Uint8>, Pointer<Uint8>, int);
-typedef _c_memcpy = Void Function(Pointer<Uint8>, Pointer<Uint8>, IntPtr);
+typedef _DartMemcpy = void Function(Pointer<Uint8>, Pointer<Uint8>, int);
+typedef _CMemcpy = Void Function(Pointer<Uint8>, Pointer<Uint8>, IntPtr);
 
 final DynamicLibrary _stdlib = Platform.isWindows // no .process() on windows
     ? DynamicLibrary.open('vcruntime140.dll') // required by objectbox.dll
