ac7250086fceb9e3cce9267982dd970091f0a9cd
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2023-06-12 16:18:10 +0200

    Vectors: remove query support for new vector properties #65

--- objectbox_test/test/query_scalar_vector_test.dart
+++ objectbox_test/test/query_scalar_vector_test.dart
@@ -24,20 +24,18 @@ void main() {
       TestEntityScalarVectors_.tLongList
     ];
 
-    final params5 = [14, 1004, 100004, 10000000004];
-
     for (int i = 0; i < properties.length; i++) {
       var property = properties[i];
-      var param5 = params5[i];
 
-      // "greater" which behaves like "has element greater".
-      // FIXME Fails with "Property is not a integer scalar type: ShortVector (OBX_ERROR code 10002)"
-      final query =
-          box.query(property.greaterThan(100 * 1000 * 1000000)).build();
-      expect(query.findIds(), []);
-      query.param(property).value = param5;
-      expect(query.findIds(), [6, 7, 8, 9, 10]);
-      query.close();
+      // Note: regular conditions currently not supported by C library.
+
+      final queryNull = box.query(property.isNull()).build();
+      expect(queryNull.findIds(), []);
+      queryNull.close();
+
+      final queryNotNull = box.query(property.notNull()).build();
+      expect(queryNotNull.findIds(), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      queryNotNull.close();
     }
   });
 
@@ -52,19 +50,18 @@ void main() {
       TestEntityScalarVectors_.tDoubleList
     ];
 
-    final params5 = [20.4, 2000.00004];
-
     for (int i = 0; i < properties.length; i++) {
       var property = properties[i];
-      var param5 = params5[i];
 
-      // "greater" which behaves like "has element greater".
-      // FIXME Fails with "Property is not a floating point scalar type: FloatVector (OBX_ERROR code 10002)"
-      final query = box.query(property.greaterThan(2001.0)).build();
-      expect(query.findIds(), []);
-      query.param(property).value = param5;
-      expect(query.findIds(), [6, 7, 8, 9, 10]);
-      query.close();
+      // Note: regular conditions currently not supported by C library.
+
+      final queryNull = box.query(property.isNull()).build();
+      expect(queryNull.findIds(), []);
+      queryNull.close();
+
+      final queryNotNull = box.query(property.notNull()).build();
+      expect(queryNotNull.findIds(), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+      queryNotNull.close();
     }
   });
 }
