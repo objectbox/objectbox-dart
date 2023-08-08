4b3a7e752735afe4892fb0cc4733d3d3496b3f5e
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2023-08-08 10:08:10 +0200

    Benchmarks: update asTypedList vs. memcpy results. Drop unused test.

--- benchmark/bin/native_pointers.dart
+++ benchmark/bin/native_pointers.dart
@@ -5,27 +5,24 @@ import 'package:ffi/ffi.dart';
 import 'package:objectbox/src/native/bindings/nativemem.dart';
 import 'package:objectbox_benchmark/benchmark.dart';
 
-// Results (Dart SDK 2.12):
-// $ dart run bin/native_pointers.dart
-// AsTypedList[size=256](Single iteration): 0.1300 us
-// AsTypedList[size=256](Runtime per unit): 0.1300 us
-// AsTypedList[size=256](Runs per second):  7 692 912
-// AsTypedList[size=256](Units per second): 7 692 912
-// AsTypedListUint64[size=32](Single iteration): 0.1402 us
-// AsTypedListUint64[size=32](Runtime per unit): 0.1402 us
-// AsTypedListUint64[size=32](Runs per second):  7 131 882
-// AsTypedListUint64[size=32](Units per second): 7 131 882
+// Results
+// Dart 3.0.5 (Flutter 3.10.5)
+// Ubuntu 22.04.3 (WSL2, AMD Ryzen 7 5700X, 8-Core, 3.40 GHz, Uwe)
 //
-// A quick profiling session shows .asTypedList() taking about 90% of the time,
-// which is consistent with profiling objectbox-dart Box.read().
+// $ dart compile exe bin/native_pointers.dart
+// $ ./bin/native_pointers.exe
+//
+// AsTypedList[size=256](single iteration):   0.0160 us
+// TypedListMemCopy(single iteration):        0.0163 us
+//
+// AsTypedList[size=1024](single iteration):  0.0165 us
+// TypedListMemCopy(single iteration):        0.0232 us
 
+/// This compares the performance of using Dart's Pointer.asTypedList vs.
+/// manual memcpy to a buffer.
 void main() async {
   final sizeInBytes = 1024;
   await AsTypedList(sizeInBytes).report();
-
-  // just checking if using a larger underlying type would help with anything
-  // await AsTypedListUint64((sizeInBytes / 8).floor()).report();
-
   await TypedListMemCopy(sizeInBytes).report();
 }
 
@@ -45,30 +42,6 @@ class AsTypedList extends Benchmark {
 
   @override
   void setup() => nativePtr = malloc<Uint8>(length);
-
-  @override
-  void teardown() {
-    malloc.free(nativePtr);
-    super.teardown();
-  }
-}
-
-class AsTypedListUint64 extends Benchmark {
-  final int length;
-  late final Pointer<Uint64> nativePtr;
-
-  AsTypedListUint64(this.length)
-      : super('$AsTypedListUint64[size=$length]', iterations: 1000);
-
-  @override
-  void runIteration(int i) {
-    final typedList = nativePtr.asTypedList(length);
-    ByteData.view(typedList.buffer, typedList.offsetInBytes);
-    // actually using the data (read flatbuffers) doesn't matter here
-  }
-
-  @override
-  void setup() => nativePtr = malloc<Uint64>(length);
 
   @override
   void teardown() {
