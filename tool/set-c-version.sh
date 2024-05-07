48ae096517d9bfa88540c8403eaa3076fd92faf4
Uwe <13865709+greenrobot-team@users.noreply.github.com>
2024-05-07 10:35:57 +0200

    set-c-version-sh: support replacing version with suffix

--- tool/set-c-version.sh
+++ tool/set-c-version.sh
@@ -8,7 +8,7 @@
 
 if [[ "$#" -ne "1" ]]; then
   echo "usage: $0 <version>"
-  echo "e.g. $0 0.20.0"
+  echo "e.g. $0 0.21.1-alpha1"
   exit 1
 fi
 
@@ -16,11 +16,12 @@ version=$1
 
 echo "Setting cLibVersion and OBJECTBOX_VERSION version: $version"
 
-versionExpr="s/cLibVersion=[0-9]\+\.[0-9]\+\.[0-9]\+/cLibVersion=${version}/g"
+# Version like 0.1.2 or 0.1.2-alpha1
+versionExpr="s/cLibVersion=[0-9]\+\.[0-9]\+\.[0-9]\+[0-9a-zA-Z-]*/cLibVersion=${version}/g"
 update install.sh "${versionExpr}"
 update tool/update-c-binding.sh "${versionExpr}"
 
-versionExpr="s/OBJECTBOX_VERSION [0-9]\+\.[0-9]\+\.[0-9]\+/OBJECTBOX_VERSION ${version}/g"
+versionExpr="s/OBJECTBOX_VERSION [0-9]\+\.[0-9]\+\.[0-9]\+[0-9a-zA-Z-]*/OBJECTBOX_VERSION ${version}/g"
 update flutter_libs/linux/CMakeLists.txt "${versionExpr}"
 update flutter_libs/windows/CMakeLists.txt "${versionExpr}"
 update sync_flutter_libs/linux/CMakeLists.txt "${versionExpr}"
