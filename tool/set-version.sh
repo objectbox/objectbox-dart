a87c1f2edc820af8d80222c80df94139345c9daa
Uwe <noreply+uwe@objectbox.io>
2025-03-11 12:32:47 +0100

    Relations demo: better suggestions to avoid Admin library conflicts
    When updating the objectbox package, projects that use the Admin library
    typically also need to manually update that library to avoid a runtime
    error.
    However, when using a version range for the objectbox package running
    pub upgrade may silently upgrade it. So suggest using a concrete
    version and add a note to pubspec to not forget about updating the
    Admin library.
    
    This matches changes to the Admin online documentation.

--- tool/set-version.sh
+++ tool/set-version.sh
@@ -11,26 +11,31 @@ version=$1
 
 echo "Setting version: $version"
 
+# Package versions
 versionExpr="s/version: .*/version: ${version}/g"
 update objectbox/pubspec.yaml "${versionExpr}"
 update generator/pubspec.yaml "${versionExpr}"
 update flutter_libs/pubspec.yaml "${versionExpr}"
 update sync_flutter_libs/pubspec.yaml "${versionExpr}"
 
+# Version constant in generator package
 versionDartExpr="s/current = \".*\"/current = \"${version}\"/g"
 update generator/lib/src/version.dart "${versionDartExpr}"
 
+# Package dependencies using a version range with caret syntax (like "objectbox: ^1.2.3")
 dependencyHigherExpr="s/objectbox: \^.*/objectbox: ^${version}/g"
 update objectbox/example/dart-native/vectorsearch_cities/pubspec.yaml "${dependencyHigherExpr}"
 update objectbox/example/flutter/event_management_tutorial/event_manager/pubspec.yaml "${dependencyHigherExpr}"
 update objectbox/example/flutter/event_management_tutorial/many_to_many/pubspec.yaml "${dependencyHigherExpr}"
 update objectbox/example/flutter/objectbox_demo/pubspec.yaml "${dependencyHigherExpr}"
-update objectbox/example/flutter/objectbox_demo_relations/pubspec.yaml "${dependencyHigherExpr}"
 update objectbox/example/flutter/objectbox_demo_sync/pubspec.yaml "${dependencyHigherExpr}"
 
+# Package dependencies using a concrete version (like "objectbox: 1.2.3")
 dependencyExactExpr="s/objectbox: [0-9]\+.*/objectbox: ${version}/g"
+update objectbox/example/flutter/objectbox_demo_relations/pubspec.yaml "${dependencyExactExpr}"
 update generator/pubspec.yaml "${dependencyExactExpr}"
 update flutter_libs/pubspec.yaml "${dependencyExactExpr}"
 update sync_flutter_libs/pubspec.yaml "${dependencyExactExpr}"
 
+# Changelog latest title
 update objectbox/CHANGELOG.md "s/## latest.*/## ${version} ($(date -I))/g"
