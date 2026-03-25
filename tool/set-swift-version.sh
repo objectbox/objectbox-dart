6850760bbb413fe4a2c6892e0d2551790b6be4ee
Uwe <noreply+uwe@objectbox.io>
2026-03-25 07:11:23 +0100

    set-swift-version.sh: replace any version string

--- tool/set-swift-version.sh
+++ tool/set-swift-version.sh
@@ -11,10 +11,12 @@ version=$1
 
 echo "Setting ObjectBox Swift version: $version"
 
-versionExpr="s/dependency 'ObjectBox', '[0-9]\+\.[0-9]\+\.[0-9]'/dependency 'ObjectBox', '${version}'/g"
+# Regular expressions match any version string ending with a single quote ('),
+# such as 1.2.3' or 1.2.3-beta.1'
+versionExpr="s/dependency 'ObjectBox', '[^\']*'/dependency 'ObjectBox', '${version}'/g"
 update flutter_libs/ios/objectbox_flutter_libs.podspec "${versionExpr}"
 update flutter_libs/macos/objectbox_flutter_libs.podspec "${versionExpr}"
 
-versionExpr="s/dependency 'ObjectBox', '[0-9]\+\.[0-9]\+\.[0-9]-sync'/dependency 'ObjectBox', '${version}-sync'/g"
+versionExpr="s/dependency 'ObjectBox', '[^\']*'/dependency 'ObjectBox', '${version}-sync'/g"
 update sync_flutter_libs/ios/objectbox_sync_flutter_libs.podspec "${versionExpr}"
 update sync_flutter_libs/macos/objectbox_sync_flutter_libs.podspec "${versionExpr}"
