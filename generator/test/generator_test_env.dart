ba11371fac38a663e2d7203d973d3b54bdfe6996
Uwe <noreply+uwe@objectbox.io>
2026-08-19 07:05:08 +0200

    Generator (style): add list items using inline syntax

--- generator/test/generator_test_env.dart
+++ generator/test/generator_test_env.dart
@@ -109,16 +109,14 @@ class GeneratorTestEnv {
     // collect UIDs on all entities and properties
     final allUIDs = generatorModel.entities
         .map(
-          (entity) =>
-              <int>[]
-                ..add(entity.id.uid)
-                ..addAll(entity.properties.map((prop) => prop.id.uid))
-                ..addAll(
-                  entity.properties
-                      .where((prop) => prop.hasIndexFlag())
-                      .map((prop) => prop.indexId!.uid),
-                )
-                ..addAll(entity.relations.map((rel) => rel.id.uid)),
+          (entity) => <int>[
+            entity.id.uid,
+            ...entity.properties.map((prop) => prop.id.uid),
+            ...entity.properties
+                .where((prop) => prop.hasIndexFlag())
+                .map((prop) => prop.indexId!.uid),
+            ...entity.relations.map((rel) => rel.id.uid),
+          ],
         )
         .reduce((List<int> a, List<int> b) => a + b);
 
