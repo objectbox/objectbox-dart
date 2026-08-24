c6d9dd6ce0167d34e23ad3414d41623fd6d0cfba
Uwe <noreply+uwe@objectbox.io>
2026-08-24 13:14:38 +0200

    Tests (coverage): collect coverage on GitLab as part of test run
    Also make coverage.sh runnable from test package.
    
    Drop separate report generator on GitHub CI and just use the one
    generated by lcov's genhtml.

--- objectbox/tool/coverage.sh
+++ objectbox/tool/coverage.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Expects to be run from a sibling directory of the objectbox package and that
+# `dart test` with `--coverage=coverage` was run before in the working directory.
 # https://pub.dev/packages/test#collecting-code-coverage
-# Must run test in package directory for which to generate coverage.
-# Run only one test suite (== test file) at a time to prevent native errors.
-dart test ../objectbox_test --coverage-path=./coverage/lcov.info --concurrency=1
+
+# Create report for ../objectbox/lib in coverage/lcov.info
+# For options, run dart pub global run coverage:format_coverage --help
+dart pub global activate coverage
+dart pub global run coverage:format_coverage \
+  --lcov \
+  --in=coverage \
+  --out=coverage/lcov.info \
+  --report-on=../objectbox/lib
 # Exclude some files that can't be tested.
 # The pattern must be specified to match the absolute path of each source file.
 lcov --remove coverage/lcov.info '*/lib/src/native/admin.dart' '*/lib/src/native/bindings/objectbox_c.dart' '*/lib/src/native/bindings/bindings.dart' '*/lib/src/modelinfo/*' -o coverage/lcov.info
