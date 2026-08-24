123e4ae9ebb14a0af9ebd391d70363520905a7bb
Uwe <noreply+uwe@objectbox.io>
2026-08-24 12:15:06 +0200

    Tests (coverage): use new coverage-path option, adapt to improvements
    Technically, test 1.27.0 requires Dart 3.7. But making objectbox_test
    require Dart 3.7 involves reformatting code, so skipping that for now.

--- objectbox/tool/coverage.sh
+++ objectbox/tool/coverage.sh
@@ -2,11 +2,10 @@
 set -euo pipefail
 
 # https://pub.dev/packages/test#collecting-code-coverage
-# Note: Run test in package directory for which to generate coverage,
-# output is in ./objectbox_test despite specifying ./coverage.
+# Must run test in package directory for which to generate coverage.
 # Run only one test suite (== test file) at a time to prevent native errors.
-dart test ../objectbox_test --coverage=./coverage --concurrency=1
-dart pub global run coverage:format_coverage --packages=.dart_tool/package_config.json --report-on=lib --lcov -o ./coverage/lcov.info -i ./objectbox_test
+dart test ../objectbox_test --coverage-path=./coverage/lcov.info --concurrency=1
+# Exclude some files that can't be tested.
 # The pattern must be specified to match the absolute path of each source file.
 lcov --remove coverage/lcov.info '*/lib/src/native/admin.dart' '*/lib/src/native/bindings/objectbox_c.dart' '*/lib/src/native/bindings/bindings.dart' '*/lib/src/modelinfo/*' -o coverage/lcov.info
 genhtml coverage/lcov.info -o coverage/html || true
