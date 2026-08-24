4bae46ad78426c8b21af0c6dc1dc87bf0dc968f5
Uwe <noreply+uwe@objectbox.io>
2026-08-24 10:58:20 +0200

    Tests (coverage): check sync API as CI now uses Sync library for tests

--- objectbox/tool/coverage.sh
+++ objectbox/tool/coverage.sh
@@ -8,5 +8,5 @@ set -euo pipefail
 dart test ../objectbox_test --coverage=./coverage --concurrency=1
 dart pub global run coverage:format_coverage --packages=.dart_tool/package_config.json --report-on=lib --lcov -o ./coverage/lcov.info -i ./objectbox_test
 # The pattern must be specified to match the absolute path of each source file.
-lcov --remove coverage/lcov.info '*/lib/src/native/admin.dart' '*/lib/src/native/sync.dart' '*/lib/src/native/bindings/objectbox_c.dart' '*/lib/src/native/bindings/bindings.dart' '*/lib/src/modelinfo/*' -o coverage/lcov.info
+lcov --remove coverage/lcov.info '*/lib/src/native/admin.dart' '*/lib/src/native/bindings/objectbox_c.dart' '*/lib/src/native/bindings/bindings.dart' '*/lib/src/modelinfo/*' -o coverage/lcov.info
 genhtml coverage/lcov.info -o coverage/html || true
