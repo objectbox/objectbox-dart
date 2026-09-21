87973aec4e173352948d792c4ab4f661ee9cfcbf
Markus <markus@greenrobot>
2026-09-21 16:29:06 +0200

    Data visitor: use NativeCallable instead of a static callback stack
    NativeCallable.isolateLocal (Dart 3.1) accepts closures, so query
    results can be visited with the actual callback instead of a static
    wrapper that dispatches through a global stack (needed to support
    visiting a nested query from within a callback). Each visit creates and
    closes its own callable, which costs about 160 ns per query run and is
    not measurable in the query benchmark.
    Test: visit a native query directly to cover cases the public query API does
    not: a nested query run from within a callback (e.g. by an entity
    constructor or setter), cancelling and an exception escaping the
    callback.

--- objectbox/lib/src/native/bindings/data_visitor.dart
+++ objectbox/lib/src/native/bindings/data_visitor.dart
@@ -9,42 +9,13 @@ typedef VisitCallback = bool Function(Pointer<Uint8> data, int size);
 /// Callback for reading query results one-by-one, see [visitWithScore].
 typedef VisitWithScoreCallback = bool Function(Pointer<OBX_bytes_score> data);
 
-/// Currently FFI's Pointer.fromFunction only allows to pass a static Dart
-/// callback function. When passing a closure it would throw at runtime:
-///     Error: fromFunction expects a static function as parameter.
-///     dart:ffi only supports calling static Dart functions from native code.
-///     Closures and tear-offs are not supported because they can capture context.
-///
-/// So given that and
-/// - it is required that each query has its own callback,
-/// - it is possible that as part of visiting results another query is created
-///   and visits its results (e.g. query run in entity constructor or setter) and
-/// - Dart code within an isolate is executed synchronously:
-///
-/// Create a single static callback function [_visitCallbackWrapper] that wraps
-/// the actual Dart callback of the query currently visiting results.
-/// Keep callbacks on a [_visitCallbackStack] to restore the callback of an outer
-/// query once a nested query is finished visiting results.
-List<VisitCallback> _visitCallbackStack = [];
+/// Native signature of [obx_data_visitor].
+typedef _DataVisitorNative =
+    Bool Function(Pointer<Uint8> data, Size size, Pointer<Void> userData);
 
-/// Like [_visitCallbackStack], but for [VisitWithScoreCallback].
-List<VisitWithScoreCallback> _visitWithScoreCallbackStack = [];
-
-bool _visitCallbackWrapper(Pointer<Uint8> dataPtr, int size, Pointer<Void> _) =>
-    _visitCallbackStack.last(dataPtr, size);
-
-bool _visitWithScoreCallbackWrapper(
-  Pointer<OBX_bytes_score> dataPtr,
-  Pointer<Void> _,
-) => _visitWithScoreCallbackStack.last(dataPtr);
-
-final Pointer<obx_data_visitor> _visitCallbackWrapperPtr = Pointer.fromFunction(
-  _visitCallbackWrapper,
-  false,
-);
-
-final Pointer<obx_data_score_visitor> _visitWithScoreCallbackWrapperPtr =
-    Pointer.fromFunction(_visitWithScoreCallbackWrapper, false);
+/// Native signature of [obx_data_score_visitor].
+typedef _DataScoreVisitorNative =
+    Bool Function(Pointer<OBX_bytes_score> data, Pointer<Void> userData);
 
 /// Visits query results to read results one by one (in chunks).
 ///
@@ -56,14 +27,22 @@ final Pointer<obx_data_score_visitor> _visitWithScoreCallbackWrapperPtr =
 /// - Return true to keep going, false to cancel.
 ///
 /// Use [ObjectVisitorError] to get an error out of the callback.
+///
+/// The [callback] may be a closure and may itself create and visit another
+/// query (e.g. a query run in an entity constructor or setter): each call wraps
+/// it in its own [NativeCallable] that only lives for the duration of the call.
 @pragma('vm:prefer-inline')
 void visit(Pointer<OBX_query> queryPtr, VisitCallback callback) {
-  // Keep callback in case another query is created and visits results
-  // within the callback.
-  _visitCallbackStack.add(callback);
-  final code = C.query_visit(queryPtr, _visitCallbackWrapperPtr, nullptr);
-  _visitCallbackStack.removeLast();
-  // Clean callback from stack before potentially throwing.
+  final callable = NativeCallable<_DataVisitorNative>.isolateLocal(
+    (Pointer<Uint8> data, int size, Pointer<Void> _) => callback(data, size),
+    exceptionalReturn: false,
+  );
+  final int code;
+  try {
+    code = C.query_visit(queryPtr, callable.nativeFunction, nullptr);
+  } finally {
+    callable.close();
+  }
   checkObx(code);
 }
 
@@ -77,21 +56,23 @@ void visit(Pointer<OBX_query> queryPtr, VisitCallback callback) {
 /// - Return true to keep going, false to cancel.
 ///
 /// Use [ObjectVisitorError] to get an error out of the callback.
+///
+/// See [visit] for details on the callback.
 @pragma('vm:prefer-inline')
 void visitWithScore(
   Pointer<OBX_query> queryPtr,
   VisitWithScoreCallback callback,
 ) {
-  // Keep callback in case another query is created and visits results
-  // within the callback.
-  _visitWithScoreCallbackStack.add(callback);
-  final code = C.query_visit_with_score(
-    queryPtr,
-    _visitWithScoreCallbackWrapperPtr,
-    nullptr,
+  final callable = NativeCallable<_DataScoreVisitorNative>.isolateLocal(
+    (Pointer<OBX_bytes_score> data, Pointer<Void> _) => callback(data),
+    exceptionalReturn: false,
   );
-  _visitWithScoreCallbackStack.removeLast();
-  // Clean callback from stack before potentially throwing.
+  final int code;
+  try {
+    code = C.query_visit_with_score(queryPtr, callable.nativeFunction, nullptr);
+  } finally {
+    callable.close();
+  }
   checkObx(code);
 }
 
---
+++ objectbox_test/test/data_visitor_test.dart
@@ -0,0 +1,96 @@
+import 'dart:ffi';
+
+import 'package:objectbox/src/native/bindings/bindings.dart';
+import 'package:objectbox/src/native/bindings/data_visitor.dart';
+import 'package:objectbox/src/native/bindings/helpers.dart';
+import 'package:objectbox/src/store.dart';
+import 'package:test/test.dart';
+
+import 'entity.dart';
+import 'objectbox.g.dart';
+import 'test_env.dart';
+
+/// Tests [visit] directly with a native query to cover cases the public query
+/// API does not: a nested query run from within a callback (e.g. by an entity
+/// constructor or setter), cancelling and throwing.
+void main() {
+  late TestEnv env;
+  late Box<TestEntity> box;
+
+  setUp(() {
+    env = TestEnv('data_visitor');
+    box = env.box;
+    box.putMany([
+      TestEntity(tString: 'a'),
+      TestEntity(tString: 'b'),
+      TestEntity(tString: 'c'),
+    ]);
+  });
+
+  tearDown(() => env.closeAndDelete());
+
+  /// Builds a native query matching all objects, caller must close it.
+  Pointer<OBX_query> buildQueryAll() {
+    final entityId =
+        InternalStoreAccess.entityDef<TestEntity>(env.store).model.id.id;
+    final builder = checkObxPtr(
+      C.query_builder(InternalStoreAccess.cStore(env.store), entityId),
+    );
+    final query = checkObxPtr(C.query(builder));
+    checkObx(C.qb_close(builder));
+    return query;
+  }
+
+  test('nested query within callback', () {
+    final query = buildQueryAll();
+    try {
+      var visited = 0;
+      var nestedResults = 0;
+      visit(query, (data, size) {
+        visited++;
+        final nestedQuery =
+            box.query(TestEntity_.tString.notEquals('')).build();
+        try {
+          nestedResults += nestedQuery.find().length;
+        } finally {
+          nestedQuery.close();
+        }
+        return true;
+      });
+      expect(visited, 3);
+      expect(nestedResults, 9);
+    } finally {
+      checkObx(C.query_close(query));
+    }
+  });
+
+  test('cancel visiting', () {
+    final query = buildQueryAll();
+    try {
+      var visited = 0;
+      visit(query, (data, size) {
+        visited++;
+        return false;
+      });
+      expect(visited, 1);
+    } finally {
+      checkObx(C.query_close(query));
+    }
+  });
+
+  test('exception in callback cancels visiting', () {
+    final query = buildQueryAll();
+    try {
+      var visited = 0;
+      // The exception does not propagate (the native callback returns false),
+      // so callers use ObjectVisitorError to get it out.
+      visit(query, (data, size) {
+        visited++;
+        throw StateError('inside callback');
+      });
+      expect(visited, 1);
+    } finally {
+      checkObx(C.query_close(query));
+    }
+  });
+}
